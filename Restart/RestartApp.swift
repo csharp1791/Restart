0f248a652db283b149a07b1341a5002997952e4b
Clifford Sharp <csharpusa@gmail.com>
2023-01-25 14:01:37 -0600

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Cliff Sharp on 1/25/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
